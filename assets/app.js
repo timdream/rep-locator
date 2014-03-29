af6c49e2d7906bafcdf98a3b7d220836b2af1ab2
Timothy Guan-tin Chien <timdream@gmail.com>
2014-03-29 18:48:47 +0800

    Move selector functions to RepSelector

--- assets/app.js
+++ assets/app.js
@@ -9,9 +9,8 @@
     this.$selectors = [];
     this.locationResolver = null;
     this.repCard = null;
+    this.repSelector = null;
   }
-
-  RepLocator.prototype.ADDRESS_SELECTOR_ID_PREFIX = 'address-selector';
 
   RepLocator.prototype.start = function() {
     if (this._started) {
@@ -29,7 +28,8 @@
     this.repCard = new RepCard(data);
     this.repCard.start();
 
-    this.titlePrefix = window.document.title;
+    this.repSelector = new RepSelector(this);
+    this.repSelector.start();
   };
 
   RepLocator.prototype.stop = function hb_stop() {
@@ -50,7 +50,8 @@
     this.repCard.stop();
     this.repCard = null;
 
-    window.document.title = this.titlePrefix;
+    this.repSelector.stop();
+    this.repSelector = null;
   };
 
   RepLocator.prototype.handleEvent = function(evt) {
@@ -59,28 +60,8 @@
   };
 
   RepLocator.prototype.handleDataReady = function() {
-    var $selectors = this.$selectors;
-
-    var $selector;
-    for (var i = 0; i < 3; i++) {
-      $selector = $('#' + this.ADDRESS_SELECTOR_ID_PREFIX + i);
-      $selector.append('<option />');
-      $selectors.push($selector);
-    }
-
-    this.data.getTopLevelNames().forEach(function(name) {
-      var $o = $('<option />');
-      $o.text(name);
-      $o.val(name);
-      $selectors[0].append($o);
-    }, this);
-
-    $selectors[0].prop('disabled', false)
-      .on('change', this.handleTopSelect.bind(this));
-    $selectors[1].prop('disabled', true)
-      .on('change', this.handle2ndLevelSelect.bind(this));
-    $selectors[2].prop('disabled', true)
-      .on('change', this.handle3ndLevelSelect.bind(this));
+    this.repSelector.enableSelectors();
+    this.locationResolver.enableButton();
 
     if (window.location.hash.substr(1)) {
       this.showRepFromHash();
@@ -88,8 +69,6 @@
     }
 
     window.addEventListener('hashchange', this);
-
-    this.locationResolver.enableButton();
   };
 
   RepLocator.prototype.updateLocation = function(addressPrefix) {
@@ -99,111 +78,7 @@
 
   RepLocator.prototype.updateSelectedFromHash = function() {
     var addressPrefix = window.decodeURI(window.location.hash.substr(1));
-    var addressComponents = addressPrefix.split(',');
-
-    if (addressComponents.length) {
-      window.document.title = this.titlePrefix + '：' + addressComponents.join('');
-    } else {
-      window.document.title = this.titlePrefix;
-    }
-
-    var $selectors = this.$selectors;
-    $selectors[0][0].selectedIndex =
-      $selectors[0].find('option[value="' + addressComponents[0] + '"]').index();
-    this.handleTopSelect(false);
-    if (addressComponents[1]) {
-      $selectors[1][0].selectedIndex =
-        $selectors[1].find('option[value="' + addressComponents[1] + '"]').index();
-    }
-    this.handle2ndLevelSelect(false);
-    if (addressComponents[2]) {
-      $selectors[2][0].selectedIndex =
-        $selectors[2].find('option[value="' + addressComponents[2] + '"]').index();
-    }
-    this.handle3ndLevelSelect(false);
-  };
-
-  RepLocator.prototype.handleTopSelect = function(updateHash) {
-    var $selectors = this.$selectors;
-    if (!$selectors[0].val()) {
-      $selectors[1].html('<option />');
-      $selectors[1].prop('disabled', true);
-      $selectors[2].html('<option />');
-      $selectors[2].prop('disabled', true);
-
-      if (updateHash) {
-        this.updateHashFromSelected();
-      }
-      return;
-    }
-
-    var names =
-      this.data.getLocalitiesFromAddressPrefix($selectors[0].val());
-
-    $selectors[1].html('<option />');
-    $selectors[1].prop('disabled', !names.length);
-    $selectors[2].html('<option />');
-    $selectors[2].prop('disabled', true);
-
-    names.forEach(function(name) {
-      var $o = $('<option />');
-      $o.text(name);
-      $o.val(name);
-      $selectors[1].append($o);
-    }, this);
-
-    if (updateHash) {
-      this.updateHashFromSelected();
-    }
-  };
-
-  RepLocator.prototype.updateHashFromSelected = function() {
-    var addressComponents = [];
-    this.$selectors.forEach(function($selector) {
-      if ($selector.val()) {
-        addressComponents.push($selector.val());
-      }
-    }, this);
-    this.updateLocation(addressComponents.join(','));
-  };
-
-  RepLocator.prototype.handle2ndLevelSelect = function(updateHash) {
-    var $selectors = this.$selectors;
-    if (!$selectors[1].val()) {
-      $selectors[2].html('<option />');
-      $selectors[2].prop('disabled', true);
-
-      if (updateHash) {
-        this.updateHashFromSelected();
-      }
-      return;
-    }
-
-    var names =
-      this.data.getLocalitiesFromAddressPrefix(
-        $selectors[0].val() + ',' + $selectors[1].val());
-
-    $selectors[2].html('<option />');
-    $selectors[2].prop('disabled', !names.length);
-
-    names.forEach(function(name) {
-      var $o = $('<option />');
-      $o.text(name);
-      $o.val(name);
-      $selectors[2].append($o);
-    }, this);
-
-    if (updateHash) {
-      this.updateHashFromSelected();
-    }
-  };
-
-  RepLocator.prototype.handle3ndLevelSelect = function(updateHash) {
-    var $selectors = this.$selectors;
-
-    if (updateHash) {
-      this.updateHashFromSelected();
-    }
+    this.repSelector.updateSelectorLocation(addressPrefix);
   };
 
   RepLocator.prototype.showRepFromHash = function() {
---
+++ assets/rep_selector.js
@@ -0,0 +1,171 @@
+'use strict';
+
+(function(exports) {
+  function RepSelector(app) {
+    this._started = false;
+    this.$selectors = [];
+
+    this.app = app;
+  }
+
+  RepSelector.prototype.ADDRESS_SELECTOR_ID_PREFIX = 'address-selector';
+
+  RepSelector.prototype.start = function() {
+    if (this._started) {
+      throw 'Instance should not be start()\'ed twice.';
+    }
+    this._started = true;
+
+    var $selectors = this.$selectors;
+
+    var $selector;
+    for (var i = 0; i < 3; i++) {
+      $selector = $('#' + this.ADDRESS_SELECTOR_ID_PREFIX + i);
+      $selector.append('<option />');
+      $selectors.push($selector);
+    }
+
+    this.titlePrefix = window.document.title;
+  };
+
+  RepSelector.prototype.stop = function hb_stop() {
+    if (!this._started) {
+      throw 'Instance was never start()\'ed but stop() is called.';
+    }
+    this._started = false;
+
+    this.$selectors.forEach(function($s) {
+      $s.off('change').prop('disabled', true);
+    });
+    this.$selectors = [];
+
+    window.document.title = this.titlePrefix;
+  };
+
+  RepSelector.prototype.enableSelectors = function() {
+    var $selectors = this.$selectors;
+
+    this.app.data.getTopLevelNames().forEach(function(name) {
+      var $o = $('<option />');
+      $o.text(name);
+      $o.val(name);
+      $selectors[0].append($o);
+    }, this);
+
+    $selectors[0].prop('disabled', false)
+      .on('change', this.handleTopSelect.bind(this));
+    $selectors[1].prop('disabled', true)
+      .on('change', this.handle2ndLevelSelect.bind(this));
+    $selectors[2].prop('disabled', true)
+      .on('change', this.handle3ndLevelSelect.bind(this));
+  };
+
+  RepSelector.prototype.updateSelectorLocation = function(addressPrefix) {
+    var addressComponents = addressPrefix.split(',');
+
+    if (addressComponents.length) {
+      window.document.title = this.titlePrefix + '：' + addressComponents.join('');
+    } else {
+      window.document.title = this.titlePrefix;
+    }
+
+    var $selectors = this.$selectors;
+    $selectors[0][0].selectedIndex =
+      $selectors[0].find('option[value="' + addressComponents[0] + '"]').index();
+    this.handleTopSelect(false);
+    if (addressComponents[1]) {
+      $selectors[1][0].selectedIndex =
+        $selectors[1].find('option[value="' + addressComponents[1] + '"]').index();
+    }
+    this.handle2ndLevelSelect(false);
+    if (addressComponents[2]) {
+      $selectors[2][0].selectedIndex =
+        $selectors[2].find('option[value="' + addressComponents[2] + '"]').index();
+    }
+    this.handle3ndLevelSelect(false);
+  };
+
+  RepSelector.prototype.updateLocationFromSelected = function() {
+    var addressComponents = [];
+    this.$selectors.forEach(function($selector) {
+      if ($selector.val()) {
+        addressComponents.push($selector.val());
+      }
+    }, this);
+    this.app.updateLocation(addressComponents.join(','));
+  };
+
+  RepSelector.prototype.handleTopSelect = function(updateHash) {
+    var $selectors = this.$selectors;
+    if (!$selectors[0].val()) {
+      $selectors[1].html('<option />');
+      $selectors[1].prop('disabled', true);
+      $selectors[2].html('<option />');
+      $selectors[2].prop('disabled', true);
+
+      if (updateHash) {
+        this.updateLocationFromSelected();
+      }
+      return;
+    }
+
+    var names =
+      this.app.data.getLocalitiesFromAddressPrefix($selectors[0].val());
+
+    $selectors[1].html('<option />');
+    $selectors[1].prop('disabled', !names.length);
+    $selectors[2].html('<option />');
+    $selectors[2].prop('disabled', true);
+
+    names.forEach(function(name) {
+      var $o = $('<option />');
+      $o.text(name);
+      $o.val(name);
+      $selectors[1].append($o);
+    }, this);
+
+    if (updateHash) {
+      this.updateLocationFromSelected();
+    }
+  };
+
+  RepSelector.prototype.handle2ndLevelSelect = function(updateHash) {
+    var $selectors = this.$selectors;
+    if (!$selectors[1].val()) {
+      $selectors[2].html('<option />');
+      $selectors[2].prop('disabled', true);
+
+      if (updateHash) {
+        this.updateLocationFromSelected();
+      }
+      return;
+    }
+
+    var names = this.app.data.getLocalitiesFromAddressPrefix(
+      $selectors[0].val() + ',' + $selectors[1].val());
+
+    $selectors[2].html('<option />');
+    $selectors[2].prop('disabled', !names.length);
+
+    names.forEach(function(name) {
+      var $o = $('<option />');
+      $o.text(name);
+      $o.val(name);
+      $selectors[2].append($o);
+    }, this);
+
+    if (updateHash) {
+      this.updateLocationFromSelected();
+    }
+  };
+
+  RepSelector.prototype.handle3ndLevelSelect = function(updateHash) {
+    var $selectors = this.$selectors;
+
+    if (updateHash) {
+      this.updateLocationFromSelected();
+    }
+  };
+
+  exports.RepSelector = RepSelector;
+}(window));
